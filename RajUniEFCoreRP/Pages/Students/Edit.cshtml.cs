12e086dda2413a511cb1ba35f3888d25b08def1e
Md. Arafat Rahman Rana <aranab.dba@gmail.com>
2018-07-25 21:28:36 +0600

    Create Model:
    1. Created models and Db Context model.
    2. Ran the scaffold to created CRUD pages of student model
    3. Sql server connection parameters defined in appsettings.json file
    4. Created DB service and defined to Statup Class inside
    ConfigureServices method which is also known as DI container.
    5. Modified the main method by creating a new service scope and calling
    the EnsureCreated method of EFcore that the db always exist when the app
    is running.
    6. Added DbInitiazer for seeding the DB.

---
+++ RajUniEFCoreRP/Pages/Students/Edit.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RajUniEFCoreRP.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RajUniEFCoreRP.Pages.Students
+{
+    public class EditModel : PageModel
+    {
+        private readonly SchoolContext _context;
+
+        public EditModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(Student.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Student.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ RajUniEFCoreRP/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RajUniEFCoreRP.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RajUniEFCoreRP.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SchoolContext _context;
+
+        public IndexModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Student.ToListAsync();
+        }
+    }
+}
